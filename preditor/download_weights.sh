151d50d8c8ad90f1eb43a1ccd91a6659f747532e
ArtemYurchenkoo <61950754+ArtemYurchenkoo@users.noreply.github.com>
2024-04-12 12:41:53 +0300

    delete download weights script

--- preditor/download_weights.sh
+++
@@ -1,3 +0,0 @@
-wget https://huggingface.co/laion/DALLE2-PyTorch/resolve/main/decoder/1.5B/latest.pth -O decoder.pth
-wget https://huggingface.co/laion/DALLE2-PyTorch/raw/main/decoder/1.5B/decoder_config.json -O decoder_config.json
-wget https://huggingface.co/nousr/conditioned-prior/resolve/main/vit-l-14/prior_aes_finetune.pth -O prior.pth
